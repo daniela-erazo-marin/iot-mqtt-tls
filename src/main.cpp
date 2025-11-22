d4511b8a40a1ca7390273571b12600bc2c6cf915
Daniela E <daniela.erazo01@uceva.edu.co>
2025-11-21 23:49:38 -0500

    feat(main): version 1

--- src/main.cpp
+++ src/main.cpp
@@ -48,7 +48,7 @@ void setup() {
   String firmwareVersion = getFirmwareVersion();
   Serial.println("\n");
   Serial.println("========================================");
-  Serial.println("  IoT MQTT TLS Device. ...");
+  Serial.println("  IoT MQTT TLS Device. .....v1");
   Serial.print("  Firmware Version: ");
   Serial.println(firmwareVersion);
   Serial.println("========================================");
